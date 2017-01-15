099771189b5831c7690ed2b6d1fb48208ec0ed2e
Anush Mattapalli <matanatr96@gmail.com>
2017-01-15 12:45:55 -0500

    iitial commit

---
+++ plot2.R
@@ -0,0 +1,19 @@
+# Load the data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+baltimoreNEI <- NEI[NEI$fips=="24510",]
+
+aggTotalsBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+
+png("plot2.png",width=480,height=480,units="px",bg="transparent")
+
+barplot(
+  aggTotalsBaltimore$Emissions,
+  names.arg=aggTotalsBaltimore$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (Tons)",
+  main="Total PM2.5 emissions from all Baltimore City Sources"
+)
+
+dev.off()
